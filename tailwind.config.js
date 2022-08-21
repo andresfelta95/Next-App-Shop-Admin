1d1d57581acf51bb4924d9f2a1074d9a6d50f8f6
andresfelta95 <andresfelta95@gmail.com>
2022-08-21 08:39:00 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*{html,js,jsx}'],
+  darkMode: false,
+  theme: {
+    colors: {
+      ...colors,
+    },    
+  },
+};
